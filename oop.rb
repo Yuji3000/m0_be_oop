fe166d8befcaa154b3f41d44bd185ed47ba4691b
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-07-26 13:28:28 -0600

    add eval and third exercise

--- oop.rb
+++ oop.rb
@@ -2,14 +2,46 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn
+    def initialize (name, color)
+      @name = name
+      @color = "silver"
+    end
 
 
+def say
+  "*~* #{@name} is #{@color} *~*"
+  end
+end
+
+unicorn1 = Unicorn.new("Rainy", @color)
+
+p unicorn1.say
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+  attr_reader :name, :pet, :thirsty, :drink_method
+  def initialize(name, pet, thirsty, drink_method)
+    @name = name
+    @pet = "bat"
+    @thirsty = true
+    @drink_method = drink_method
+  end
+
+  def drinks
+    @thirsty = false
+    end
+  end
+
+
+
+vampire1 = Vampire.new("Jojo", @pet, @thirsty, @drink_method)
+
+p vampire1.drinks
 
 
 #  Write a Dragon class
@@ -18,8 +50,24 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+  attr_reader :name, :rider, :color, :is_hungry, :eats
+  def initialize(name, rider, color, is_hungry, eats)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = true
+    @eats = eats
+  end
+  def eat
+    eats = 4
+    is_hungry = false
+  end
+end
 
+dragon1 = Dragon.new("Fire guy", "The dragon rider", "Blue", @is_hungry, 4)
 
+p dragon1.eat
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
 #  it should have a dynamic disposition attribute (string)
@@ -28,3 +76,18 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+  attr_reader :name, :disposition, :age, :is_adult, :is_old, :has_ring
+def initialize(name, disposition, age, is_adult, is_old, has_ring)
+  @name = name
+  @disposition = disposition
+  @age = 0
+  @is_adult = false
+  @is_old = false
+  @has_ring = @has_ring
+end
+
+  def celebrate_birthday
+    age + 1
+  end
+end
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,12 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+It is challenging not knowing if I am going in the right direction or not. Sometimes it is difficult to find hints through google.
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+Workflow is going well. If I hit a road block and I can't find an answer within ~10 min I take a small break and try again.
+
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+Need help with how to google to find answers. 
